4ade269576247909d412e29e3c08264659082e22
Rishie Kandala <rishiekandala@gmail.com>
2025-02-07 12:05:20 -0500

    Time: 0 ms (100%), Space: 10.2 MB (98.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int count[256] = {0};
-        int l = 0;
-        int r = 0;
-        int ans = 0;
-        while(r < s.length()){
-            count[s[r]]++;
-            while(count[s[r]] > 1){
-                count[s[l]]--;
-                l++;
-            }
-            ans = max(ans, r-l+1);
-            r++;
-        }
-        return ans;
+       vector<bool> count(256,0);
+       int first = 0, second = 0, len = 0;
+       while(second<s.size()){
+         while(count[s[second]])
+         {
+            count[s[first]] = 0;
+            first++;
+         } 
+         count[s[second]] = 1;
+         len = max(len, second-first+1);
+         second++;
+
+       }
+       return len;
 
         
     }
