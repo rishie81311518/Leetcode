386b756dba75be9856d7be8152d6ec7c749d19ac
Rishie Kandala <rishiekandala@gmail.com>
2025-08-13 12:49:17 -0400

    Time: 6 ms (27.44%), Space: 9 MB (14.61%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n <= 0)
-        return false;
-        while (n % 3 == 0){
-            n /= 3;
-        }
-        return n == 1;
+    const int max_Power = 1162261467;
+    return n > 0 && (max_Power % n == 0);
     }
     
 };
