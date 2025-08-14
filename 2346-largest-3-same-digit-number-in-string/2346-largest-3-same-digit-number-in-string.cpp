60d6c6c9a7bc891675177d9390f5695bc3ffa5b7
Rishie Kandala <rishiekandala@gmail.com>
2025-08-14 14:17:51 -0400

    Time: 0 ms (100%), Space: 8.8 MB (96.52%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        //better approach
+        //brute approach we know the num only contains digits 0-9
 
-        int maxDigit = -1;
-        for (int i = 0; i <= num.size() - 3; i++){
-            if(num[i] == num[i+1] && num[i+1] == num[i+2]){
-                   maxDigit = max(maxDigit, num[i] - '0');
-            }
+        for (int i = 9; i >= 0; i--){
+           string pattern(3, '0' + i);
+
+           if(num.find(pattern) != string::npos){
+               return pattern;
+           }
         }
-        if (maxDigit == -1) return "";
-        return string (3, '0' + maxDigit);
+
+        return "";
 
         
         
