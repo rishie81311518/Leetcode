40c603233d4061bb0edc73d5677789d13a6cc72d
Rishie Kandala <rishiekandala@gmail.com>
2026-01-02 16:40:42 -0500

    Time: 0 ms (100%), Space: 28.3 MB (97.62%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        unordered_map<int,int> freq;
+     int n = nums.size();
 
-        for(int num: nums){
-            freq[num]++;
-            if (freq[num] > 1){
-                return num;
-            }  
+     for(int i = 0; i < n; i++) {
+        for(int j = 1; j <= 3 && i+j<n; j++){
+            if(nums[i] == nums[i+j])
+            return nums[i];
         }
+     }
+     return -1;
 
-    return -1;
         
     }
 };
