19d7426ba016510263d50300f50d8c2347cef2fe
Rishie Kandala <rishiekandala@gmail.com>
2026-01-13 02:22:59 -0500

    Time: 0 ms (100%), Space: 13.8 MB (26.71%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        //Better
 
         int totalTime = 0;
 
         for(int i = 0; i<points.size() - 1; i++){
-            int x1 = points[i][0], y1 = points[i][1];
-            int x2 = points[i+1][0], y2 = points[i+1][1];
+           int x1 = points[i][0], y1 = points[i][1];
+           int x2 = points[i+1][0], y2 = points[i+1][1];
 
-            while( x1 != x2 || y1 != y2){
-                if (x1 != x2 && y1 != y2){
-                    x1 += (x2>x1) ? 1 : -1;
-                    y1 += (y2>y1) ? 1 : -1;
-                }
 
-                else if (x1 != x2){
-                    x1 += (x2>x1) ? 1 : -1;
-                }
+            int dx = abs(x2-x1);
+            int dy = abs(y2-y1);
 
-                else if (y1 != y2){
-                    y1 += (y2>y1) ? 1 : -1;
-                }
-               totalTime++;
-              
-            }
-                
+            int diagonalMoves = min(dx,dy);
+            int straightMoves = abs(dx-dy);
 
-           }
-           return totalTime;
-        
+            totalTime += diagonalMoves + straightMoves; 
+        }
+
+        return totalTime;
     }
+         
 };
